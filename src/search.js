5f5de5680f1118987f1e9405953b261456195e47
limengyi <1051933474@qq.com>
2020-12-01 10:13:25 +0800

    ADD 加入react配置

--- src/search.js
+++ src/search.js
@@ -1,1 +1,14 @@
-console.log('search.js')
+// console.log('search.js')
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+class Search extends React.Component {
+    render() {
+        return <div>Search Txt</div>
+    }
+}
+
+ReactDOM.render(
+    <Search />,
+    document.getElementById('root')
+)
