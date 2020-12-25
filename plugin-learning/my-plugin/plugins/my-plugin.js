7e2dc2da73220efba44f1376095d603dff19c96c
limengyi <1051933474@qq.com>
2020-12-25 11:28:28 +0800

    ADD plugin基础学习

---
+++ plugin-learning/my-plugin/plugins/my-plugin.js
@@ -0,0 +1,10 @@
+module.exports = class MyPlugin {
+  constructor(options) {
+    this.options = options;
+  }
+
+  apply(compiler) {
+    console.log('my plugin run.');
+    console.log(this.options);
+  }
+}
